5407779ade921554a9ddfc8105cc2297dcc28dc3
grim-62 <prashantdhurve6262@gmail.com>
2024-07-24 01:46:05 +0530

    first contri

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='bg-zinc-500'>
-      hello
+    <div className='bg-zinc-500 h-screen w-screen'>
+      hello 
     </div>
   )
 }
